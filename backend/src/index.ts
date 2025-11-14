fd91ef0084c9c09b431d829f941298ad17d3bb7d
Ashish-Bakshi <ashishbakshi2004@gmail.com>
2025-11-14 18:56:54 +0000

    chore(backend): update PORT value in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ import purchaseRoutes from './routes/purchase.route';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(cookieParser());
